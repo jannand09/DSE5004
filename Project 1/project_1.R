779681aed01c39657483f8cf70ae6eb712e3aaa3
jannand09 <138733341+jannand09@users.noreply.github.com>
2024-04-29 22:00:23 -0400

    Week 8 Updates

--- Project 1/project_1.R
+++ Project 1/project_1.R
@@ -3,8 +3,10 @@ library(tidyverse)
 library(caret)
 library(writexl)
 
-data <- read.csv("data/Customer_Dataset_Data.csv", na.strings = "?", stringsAsFactors = F)
-data <- data %>% column_to_rownames(., var = "CustomerID")
+original_data <- read.csv("data/Customer_Dataset_Data.csv", na.strings = "?", stringsAsFactors = F)
+original_data <- original_data %>% column_to_rownames(., var = "CustomerID")
+
+data <- original_data
 
 # Drop irrelevant columns
 data <- data[, -c(7,18,19,20,21,23,24,25,26,27,28,29,30,31,32)]
@@ -24,18 +26,36 @@ data$DataLastMonth <- parse_number(data$DataLastMonth)
 data$CardSpendMonth <- parse_number(data$CardSpendMonth)
 
 
-# Convert null values in monetary columns to 0
+# Handle null values in monetary columns
 
-data$VoiceOverTenure[is.na(data$VoiceOverTenure)] <- 0
-data$VoiceLastMonth[is.na(data$VoiceLastMonth)] <- 0
+data$VoiceOverTenure <- replace_na(data$VoiceOverTenure, median(data$VoiceOverTenure))
+data$VoiceLastMonth <- replace_na(data$VoiceLastMonth, median(data$VoiceLastMonth))
 
-data$EquipmentOverTenure[is.na(data$EquipmentOverTenure)] <- 0
-data$EquipmentLastMonth[is.na(data$EquipmentLastMonth)] <- 0
 
-data$DataOverTenure[is.na(data$DataOverTenure)] <- 0
-data$DataLastMonth[is.na(data$DataLastMonth)] <- 0
+data$VoiceOverTenure[is.na(data$VoiceOverTenure)] <- median(data$VoiceOverTenure)
+data$VoiceLastMonth[is.na(data$VoiceLastMonth)] <- median(data$VoiceLastMonth)
 
-data$CardSpendMonth[is.na(data$CardSpendMonth)] <- 0
+handle_na <- function(data, col1, col2) {
+  med_val <- median(data[[col1]], na.rm = TRUE)  # Calculate median of Column 1
+  for (i in 1:nrow(data)) {
+    if (is.na(data[i, col1])) {
+      if (data[i, col2] == "Yes") {
+        data[i, col1] <- med_val
+      } else {
+        data[i, col1] <- 0
+      }
+    }
+  }
+  return(data)
+}
+
+data <- handle_na(data, "EquipmentOverTenure", "EquipmentRental")
+data <- handle_na(data, "EquipmentLastMonth", "EquipmentRental")
+
+data <- handle_na(data, "DataOverTenure", "WirelessData")
+data <- handle_na(data, "DataLastMonth", "WirelessData")
+
+data$CardSpendMonth[is.na(data$CardSpendMonth)] <- median(data$CardSpendMonth)
 
 # Derived Feature
 
@@ -43,6 +63,9 @@ data$HHIncome <- parse_number(data$HHIncome)
 data$TotalOverTenure <- data$VoiceOverTenure + data$EquipmentOverTenure + data$DataOverTenure
 data$TotalByTenure <- data$TotalOverTenure / data$PhoneCoTenure
 data$TotalLastMonth <- data$VoiceLastMonth + data$EquipmentLastMonth + data$DataLastMonth
+
+data$LogIncome <- log10(data$HHIncome)
+data$LogTotalLastMonth <- log10(data$TotalLastMonth)
 
 
 data_occupation <- data %>%
@@ -93,3 +116,15 @@ e <- ggplot(data = data) +
   geom_histogram(aes(x=PhoneCoTenure), color="black", fill="darkred") +
   geom_vline(xintercept = mean(data$PhoneCoTenure), color="blue")
 print(e)
+
+f <- ggplot(data = data) +
+  geom_point(aes(x=LogIncome, y=LogTotalLastMonth), color="blue")
+print(f)
+
+cor(data$LogIncome, data$LogTotalLastMonth)
+
+g <- ggplot(data = data %>% select(Age, TVWatchingHours) %>%
+              group_by(Age) %>%
+              summarise(AvgTVHours = mean(TVWatchingHours))) +
+  geom_col(aes(x=Age, y=AvgTVHours), fill="forestgreen")
+print(g)
