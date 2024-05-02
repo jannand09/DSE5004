8354062d3225e9396dd1a95d9e31f1e7e8bdc6b5
jannand09 <138733341+jannand09@users.noreply.github.com>
2024-05-01 21:47:07 -0400

    Project 2 Update

---
+++ Project 2/project_2.R
@@ -0,0 +1,185 @@
+
+library(tidyverse)
+library(caret)
+library(writexl)
+
+original_data <- read.csv("data/Customer_Dataset_Data.csv", na.strings = "?", stringsAsFactors = F)
+original_data <- original_data %>% column_to_rownames(., var = "CustomerID")
+
+data <- original_data
+
+# Drop irrelevant columns
+data <- data[, -c(7,18,19,20,21,23,24,25,26,27,28,29,30,31,32)]
+
+
+# Convert monetary columns from character to numeric
+
+data$VoiceOverTenure <- parse_number(data$VoiceOverTenure)
+data$VoiceLastMonth <- parse_number(data$VoiceLastMonth)
+
+data$EquipmentOverTenure <- parse_number(data$EquipmentOverTenure)
+data$EquipmentLastMonth <- parse_number(data$EquipmentLastMonth)
+
+data$DataOverTenure <- parse_number(data$DataOverTenure)
+data$DataLastMonth <- parse_number(data$DataLastMonth)
+
+data$CardSpendMonth <- parse_number(data$CardSpendMonth)
+
+
+# Handle null values in monetary columns
+
+data$VoiceOverTenure <- replace_na(data$VoiceOverTenure, median(data$VoiceOverTenure))
+data$VoiceLastMonth <- replace_na(data$VoiceLastMonth, median(data$VoiceLastMonth))
+
+
+data$VoiceOverTenure[is.na(data$VoiceOverTenure)] <- median(data$VoiceOverTenure)
+data$VoiceLastMonth[is.na(data$VoiceLastMonth)] <- median(data$VoiceLastMonth)
+
+handle_na <- function(data, col1, col2) {
+  med_val <- median(data[[col1]], na.rm = TRUE)  # Calculate median of Column 1
+  for (i in 1:nrow(data)) {
+    if (is.na(data[i, col1])) {
+      if (data[i, col2] == "Yes") {
+        data[i, col1] <- med_val
+      } else {
+        data[i, col1] <- 0
+      }
+    }
+  }
+  return(data)
+}
+
+data <- handle_na(data, "EquipmentOverTenure", "EquipmentRental")
+data <- handle_na(data, "EquipmentLastMonth", "EquipmentRental")
+
+data <- handle_na(data, "DataOverTenure", "WirelessData")
+data <- handle_na(data, "DataLastMonth", "WirelessData")
+
+data$CardSpendMonth[is.na(data$CardSpendMonth)] <- median(data$CardSpendMonth)
+
+# Derived Feature
+
+data$HHIncome <- parse_number(data$HHIncome)
+data$TotalOverTenure <- data$VoiceOverTenure + data$EquipmentOverTenure + data$DataOverTenure
+data$TotalByTenure <- data$TotalOverTenure / data$PhoneCoTenure
+data$TotalLastMonth <- data$VoiceLastMonth + data$EquipmentLastMonth + data$DataLastMonth
+
+data$LogIncome <- log10(data$HHIncome)
+data$LogTotalLastMonth <- log10(data$TotalLastMonth)
+
+
+data_occupation <- data %>%
+  group_by(JobCategory) %>%
+  summarise(avgLastMonth = mean(TotalLastMonth))
+
+a <- ggplot(data = data_occupation) +
+  geom_col(aes(x=JobCategory, y=avgLastMonth))
+print(a)
+
+b <- ggplot(data = data) +
+  geom_histogram(aes(x=Age), bins=30, color = "black", fill = "blue")
+print(b)
+
+c <- ggplot(data = data) +
+  geom_point(aes(x=PhoneCoTenure, y=TotalLastMonth), color = "lightblue")
+print(c)
+
+
+data_equip <- data %>%
+  add_count(EquipmentRental) %>%
+  group_by(EquipmentRental,n) %>%
+  summarise(avgLastMonth = mean(TotalLastMonth))
+
+d <- ggplot(data = data %>% filter(EquipmentRental == "Yes")) +
+  geom_histogram(aes(x=Age), color="black", fill="yellow", bins=30)
+print(d)
+
+
+data_wireless <- data %>%
+  add_count(WirelessData) %>%
+  group_by(WirelessData,n) %>%
+  summarise(avgLastMonth = mean(TotalLastMonth))
+
+data <- data %>% mutate(segment = case_when(
+  EquipmentRental == "Yes" & WirelessData == "Yes" ~ 1,
+  EquipmentRental == "Yes" & WirelessData == "No" ~ 2,
+  EquipmentRental == "No" & WirelessData == "Yes" ~ 3,
+  EquipmentRental == "No" & WirelessData == "No" ~ 4,
+))
+
+data_segmented <- data %>% add_count(segment) %>%
+  group_by(segment,n) %>%
+  select_if(is.numeric) %>%
+  summarise_all("median")
+
+e <- ggplot(data = data) +
+  geom_histogram(aes(x=PhoneCoTenure), color="black", fill="darkred") +
+  geom_vline(xintercept = mean(data$PhoneCoTenure), color="blue")
+print(e)
+
+f <- ggplot(data = data) +
+  geom_point(aes(x=LogIncome, y=LogTotalLastMonth), color="blue")
+print(f)
+
+cor(data$LogIncome, data$LogTotalLastMonth)
+
+g <- ggplot(data = data %>% select(Age, TVWatchingHours) %>%
+              group_by(Age) %>%
+              summarise(AvgTVHours = mean(TVWatchingHours))) +
+  geom_col(aes(x=Age, y=AvgTVHours), fill="forestgreen")
+print(g)
+
+
+###################### Rule Based Segmentation #################################
+
+h <- ggplot(data = data) +
+  geom_histogram(aes(x=TotalLastMonth), color="black", fill="yellow") +
+  geom_vline(xintercept=mean(data$TotalLastMonth), color="blue") +
+  annotate(geom="text", x=100, y=1200, label=as.character(mean(data$TotalLastMonth)))
+print(h)
+
+
+data$LastMonthGroup <- cut(data$TotalLastMonth, breaks=c(-1,70,600), labels=c("Low", "High"))
+
+i <- ggplot(data = data) +
+  geom_histogram(aes(x=CardSpendMonth), color="black", fill="lightgreen") +
+  geom_vline(xintercept=mean(data$CardSpendMonth, na.rm = T), color="blue") +
+  scale_x_continuous(breaks = seq(0, 40000, by=2500))
+print(i)
+
+data$CardSpendGroup <- cut(data$CardSpendMonth, breaks=c(-1,5000,40000), labels=c("Low", "High"))
+
+j <- ggplot(data = data) +
+  geom_histogram(aes(x=PhoneCoTenure), color="black", fill="lightblue") +
+  geom_vline(xintercept=mean(data$PhoneCoTenure), na.rm = T, color="blue")
+print(j)
+
+data$TenureGroup <- cut(data$CardSpendMonth, breaks=c(-1,40,100), labels=c("Short", "Long"))
+
+
+data <- data %>% mutate(segment = case_when(
+  LastMonthGroup == "Low" + CardSpendGroup =="Low" + TenureGroup =="Short" ~ 1,
+  LastMonthGroup == "Low" + CardSpendGroup =="Low" + TenureGroup =="Long" ~ 2,
+  LastMonthGroup == "Low" + CardSpendGroup =="High" + TenureGroup =="Short" ~ 3,
+  LastMonthGroup == "Low" + CardSpendGroup == "High" + TenureGroup == "Long" ~ 4,
+  LastMonthGroup == "High" + CardSpendGroup =="Low" + TenureGroup =="Short" ~ 5,
+  LastMonthGroup == "High" + CardSpendGroup =="Low" + TenureGroup =="Long" ~ 6,
+  LastMonthGroup == "High" + CardSpendGroup =="High" + TenureGroup =="Short" ~ 7,
+  LastMonthGroup == "High" + CardSpendGroup == "High" + TenureGroup == "Long" ~ 8
+))
+
+data$CustomerValue <- ifelse(data$segment %in% c(4,6,7,8),"High","Low")
+data$CustomerValue <- as.factor(data$CustomerValue)
+
+
+###################### Supervised Learning Segmentation ########################
+
+
+log_reg <- glm(CustomerValue ~ TotalLastMonth + CardSpendMonth + PhoneCoTenure, data=data,
+               family = "binomial")
+
+data$probValue <- predict(log_reg, newdata = data, type="response")
+data$ValueGroup <- cut(data$probValue, breaks=c(0.0,0.50,1.0),
+                         labels=c("Low","High"))
+
+###################### Unsupervised Learning Segmentation ######################
